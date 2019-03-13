998c36518b2a177d506d4668b3e72cb7df6ec25e
Yves voirin <yves.voirin@geoimagesolutions.com>
2019-03-13 07:24:02 -0400

    initial commit

---
+++ module1/sample1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'voirin'
+__copyright__ = "Copyright 2019"
+__credits__ = ["voirin"]
+__license__ = "GPL"
+__version__ = ""
+__maintainer__ = "voirin"
+__email__ = "yves.voirin@geoimagesolutions.com"
+__status__ = "Development"
+
+print "ceci est un test"
